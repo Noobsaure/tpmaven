04d492367ef359a92c2613f8634e606f9871ec9c
Dorian Leroy <leroy.dorian@yahoo.fr>
2014-09-09 17:56:47 +0200

    First commit

---
+++ src/main/java/fr/istic/master2/App.java
@@ -0,0 +1,12 @@
+package fr.istic.master2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");       
+        // This is a comment.
+    }
+}
